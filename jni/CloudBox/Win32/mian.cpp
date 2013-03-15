0fb57d8557dfd64f3bfa827332b9120f6ba9b6ad
cloudhsu <cloudhsu921@gmail.com>
2013-03-15 14:45:47 +0800

    1. Add mouse event in win32.
    2. Refactoring link library directory.
    3. Test OK in win32.

--- jni/CloudBox/Win32/mian.cpp
+++ jni/CloudBox/Win32/mian.cpp
@@ -8,9 +8,6 @@
 #include <GL/glut.h>
 #include "../CBGameApp.h"
 
-GLenum doubleBuffer;
-GLubyte ubImage[65536];
-
 static void Init(void)
 {
   glMatrixMode(GL_PROJECTION);
@@ -20,6 +17,7 @@ static void Init(void)
   glDisable(GL_DITHER);
 
   SGameApp.initialize();
+  SGameApp.initialTimer(((double)GetTickCount())/1000.0);
 }
 
 /* ARGSUSED1 */
@@ -31,23 +29,34 @@ static void Key(unsigned char key, int x, int y)
   }
 }
 
-static void Mouse(int x, int y, int x1, int y1)
+static void Mouse(int button, int state, int x, int y)
 {
+    static bool isDrag = false;
+    if(GLUT_LEFT_BUTTON == button)
+    {
+        if(GLUT_DOWN == state && isDrag == false)
+        {
+            isDrag = true;
+            SGameApp.touchBegan(x, y);
+        }
+        else if(GLUT_UP == state)
+        {
+            isDrag = false;
+            SGameApp.touchEnded(x, y);
+        }
+        if(isDrag)
+        {
+            SGameApp.touchMoved(x, y);
+        }
+    }
 }
 
 static void Draw(void)
 {
-    /*glClearColor(0.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glRasterPos3f(0.2, -0.8, -1.5);*/
-    /*glDrawPixels(128, 128, GL_RGBA, GL_UNSIGNED_BYTE, ubImage);*/
-
-    //TexFunc();
-    double time = 0;
+    double time = ((double)GetTickCount())/1000.0;
     SGameApp.mainLoop(time);
-
     glutSwapBuffers();
+    glutPostRedisplay();
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
@@ -56,6 +65,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
   int argc = 0;
   glutInit(&argc, (char**)"");
 
+  // initial GDI+
   ULONG_PTR m_gdiplusToken;
   Gdiplus::GdiplusStartupInput gdiplusStartupInput;
   Gdiplus::GdiplusStartup(&m_gdiplusToken, &gdiplusStartupInput, NULL);
@@ -67,6 +77,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
   //glutFullScreen();  
   if (!glutExtensionSupported("GL_EXT_abgr")) {
     printf("Couldn't find abgr extension.\n");
+    // uninitial GDI+
     Gdiplus::GdiplusShutdown(m_gdiplusToken);
     exit(0);
   }
@@ -75,7 +86,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
   //glutKeyboardFunc(Key);
   glutMouseFunc(Mouse);
   glutDisplayFunc(Draw);
+  
   glutMainLoop();
+
+  // uninitial GDI+
   Gdiplus::GdiplusShutdown(m_gdiplusToken);
   return 0;             /* ANSI C requires main to return int. */
 }
